7acc0ce20c62c2fe1366659d34ff958e4e6f5d63
Xolo978 <prithvirajbanik900@gmail.com>
2026-04-05 18:43:27 +0530

    Fixed some solutions

--- W3/A3.c
+++ W3/A3.c
@@ -1,22 +1,35 @@
+#include <stdio.h>
 float findMedian(int arr1[], int arr2[], int n) {
+
   int i = 0;
   int j = 0;
   int m1 = -1;
   int m2 = -1;
-  // Only need to reach the middle of both arrays for
-  for (int count = 0; count <= n; count++) {
+  int count = 0;
+  while (i < n && j < n && count <= n) {
     m2 = m1;
-    if (i < n && j < n) {
-      if (arr1[i] < arr2[j]) {
-        m1 = arr1[i++];
-      } else {
-        m1 = arr2[j++];
-      }
-    } else if (i < n) {
+    if (arr1[i] < arr2[j]) {
       m1 = arr1[i++];
     } else {
       m1 = arr2[j++];
     }
+    count++;
   }
+  while (i < n && count <= n) {
+    m2 = m1;
+    m1 = arr1[i++];
+    count++;
+  }
+  while (j < n && count <= n) {
+    m2 = m1;
+    m1 = arr2[j++];
+    count++;
+  }
+
   return (m1 + m2) / 2.0;
+}
+int main() {
+  int arr1[] = {2, 3, 5};
+  int arr2[] = {-1, 4, 9};
+  printf("%.2f\n", findMedian(arr1, arr2, 3));
 }
