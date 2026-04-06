291b01293e01d3b00794d45c83c1fec3d173cee5
Xolo978 <prithvirajbanik900@gmail.com>
2026-04-06 13:45:51 +0530

    Prims done

--- W1/Prims.c
+++ W1/Prims.c
@@ -1,1 +1,39 @@
-void prim(int vertices, int graph[vertices][vertices]) {}
+#include <limits.h>
+#include <stdio.h>
+
+int findMinCost(int key[], int mst[]) {
+  int min = INT_MAX;
+  return min;
+}
+void printMST(int parent[], int vertices, int graph[vertices][vertices]) {
+  printf("Edge \tWeight\n");
+  for (int i = 1; i < vertices; i++)
+    printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
+}
+
+void prim(int vertices, int start, int graph[vertices][vertices]) {
+  int dist[vertices];
+  int mst[vertices];
+  int parent[vertices];
+
+  for (int i = 0; i < vertices; i++) {
+    mst[i] = 0;
+    dist[i] = INT_MAX;
+  }
+  dist[start] = 0;
+  parent[vertices] = -1;
+
+  for (int count = 0; count < vertices - 1; count++) {
+    int u = findMinCost(dist, mst);
+    mst[u] = 1;
+    // Now for the u node relax the rest
+
+    for (int v = 0; v < vertices; v++) {
+      if (graph[u][v] && mst[v] == 0 && graph[u][v] < dist[v]) {
+        parent[v] = u;
+        dist[v] = graph[u][v];
+      }
+    }
+  }
+  printMST(parent, vertices, graph);
+}
